9783eecda815b8c75f1b77d6c5e1bedaa9ac3f50
swj <1370037533@qq.com>
2017-01-05 02:48:08 +0800

    add post table

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ app.config.from_object(DevConfig)
 
 db = SQLAlchemy(app)
 
+
+class Post(db.Model):
+    id = db.Column(db.Integer(),primary_key = True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime())
+    user_id = db.Column(db.Integer(),db.ForeignKey('user.id'))
+
+    def __init__(self,title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Post '{}'>".format(self.title)
+
 class User(db.Model):
     __tablename__ = 'user_table_name'
     id = db.Column(db.Integer(),primary_key = True)
