fd1e718b445b6792107b0aff85cd8fcd71df79e1
swj <1370037533@qq.com>
2017-01-05 01:31:41 +0800

    sqlalchemy import

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from flask import Flask
 from config import DevConfig
+from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer(),primary_key = True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+
+    def __init__(self,username):
+        self.username = username
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
+
 @app.route('/')
 def home():
     return '<h1>Hello World!</h1>'
